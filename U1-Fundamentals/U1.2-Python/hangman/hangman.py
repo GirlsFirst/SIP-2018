3ce2b6340d7c3af312cacf0d7d2d1f0ebf8d735f
Khevna Shah <khevnashah@gmail.com>
2018-02-06 18:28:21 -0800

    added some more error checks, updated some prints, added 20 empty prints so the player can't see the word!

--- U1-Fundamentals/U1.2-Python/hangman/hangman.py
+++ U1-Fundamentals/U1.2-Python/hangman/hangman.py
@@ -1,4 +1,11 @@
-word = input("Type a word for someone to guess: ")
+while True:
+	word = input("Type a word for someone to guess: ")
+	word = word.lower()
+	if(word.isalpha() == False):
+		print("That's not a word")
+	else:
+		break
+
 guesses = []
 numfails = 0
 maxfails = 7
@@ -6,6 +13,9 @@ completedword = []
 
 for letter in word:
 	completedword.append("_");
+
+for idx in range(0, 20):
+	print(" ")
 
 done = False
 
@@ -16,6 +26,8 @@ while not done:
 	print("Current Word: ", completedword)
 
 	guess = input("Guess a letter: ")
+	guess = guess.lower()
+
 	if(len(guess) > 1):
 		print("That's too long!")
 	elif(guess.isalpha() == False):
@@ -26,7 +38,7 @@ while not done:
 		guesses.append(guess)
 
 		if(guess in word):
-			print("you got a letter!")
+			print("You got a letter!")
 			for idx in range(0, len(word)):
 				if word[idx] == guess:
 					completedword[idx] = guess
@@ -39,7 +51,7 @@ while not done:
 			if done:
 				print("You won!")
 		else:
-			print("wrong guess!")
+			print("Wrong guess!")
 			numfails += 1
 
 			if numfails >= maxfails:
