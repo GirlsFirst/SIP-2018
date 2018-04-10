cd0f8d29eff4e918a979f0eaa81da3c5a444eef0
Megan Sullivan <meganesulli@gmail.com>
2018-04-10 15:53:31 -0700

    Update to include extension

---
+++ U2-Applications/U2.3-Data/DictionaryAttack/README.md
@@ -0,0 +1,9 @@
+# Dictionary Attack
+
+To run on Mac:
+`$ python3 dictionary_attack.py`
+
+To run on Windows:
+`$ python dictionary_attack.py`
+
+This program runs a simple check to see if a password matches a word in a dictionary.
--- U2-Applications/U2.3-Data/DictionaryAttack/dictionary_attack.py
+++ U2-Applications/U2.3-Data/DictionaryAttack/dictionary_attack.py
@@ -1,16 +1,37 @@
-f = open("dictionary.txt","r")
-in_dictionary = False
+def main():
 
+  f = open("dictionary.txt","r")
+  in_dictionary = False
 
-print("Can your password survive a dictionary attack?")
-test_password = input("Type in a trial password: ")
+  print("Can your password survive a dictionary attack?")
+  test_password = input("Type in a trial password: ")
 
-for line in f:
-        if line.strip() == test_password.strip():
-                in_dictionary = True
-                break
+  for line in f:
+    test_password = find_variations(test_password)
+    if line.strip() == test_password.strip():
+      in_dictionary = True
+      print("Password match found: ", line.strip())
+      break
 
-if in_dictionary:
-        print("Found password")
-else:
-        print("Password not found... in this dictionary attack")
+  if not in_dictionary:
+    print("Password not found... in this dictionary attack")
+
+# Check and convert common letter-to-number substitutions
+def find_variations(password):
+  password = password.replace("1", "l")
+  password = password.replace("!", "i")
+  password = password.replace("2", "z")
+  password = password.replace("3", "e")
+  password = password.replace("@", "a")
+  password = password.replace("4", "a")
+  password = password.replace("$", "s")
+  password = password.replace("5", "s")
+  password = password.replace("6", "g")
+  password = password.replace("7", "t")
+  password = password.replace("8", "b")
+  password = password.replace("9", "g")
+  password = password.replace("0", "o")
+  return password
+
+if __name__ == '__main__':
+  main()
